1f38588f774c828b9dfa69db5271af0437455b50
정종문 <whdans4005@naver.com>
2024-04-09 20:09:16 +0900

    feat: 게시판 UI & 게시판 버튼 토글

--- Assets/Scripts/IngameManager.cs
+++ Assets/Scripts/IngameManager.cs
@@ -4,10 +4,10 @@ using UnityEngine.UI;
 
 public class IngameManager : MonoBehaviour
 {
-    public GameObject boy, girl;
+    public GameObject boy, girl, noticeboard;
     public Transform followcam_boy, followcam_girl;
     public MainCamera mainCamera;
-    public Button btn_back;
+    public Button btn_back, btn_noticeboard, btn_noticeboard_close;
 
     void Start()
     {
@@ -22,9 +22,15 @@ public class IngameManager : MonoBehaviour
             mainCamera.target = followcam_girl;
         }
         btn_back.onClick.AddListener(LoadSelectScene);
+        btn_noticeboard.onClick.AddListener(ToggleNoticeBoard);
+        btn_noticeboard_close.onClick.AddListener(ToggleNoticeBoard);
     }
     void LoadSelectScene()
     {
         SceneManager.LoadScene("SelectScene");
+    }
+    void ToggleNoticeBoard()
+    {
+        noticeboard.SetActive(!noticeboard.activeSelf);
     }
 }
